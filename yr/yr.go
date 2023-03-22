305002780e680b8d338c82d24c2a4fae3ad069fc
naausicaa <martinsteiro02@gmail.com>
2023-03-22 14:35:54 +0100

    test for filecount

--- yr/yr.go
+++ yr/yr.go
@@ -1,5 +1,31 @@
 package yr
 
-func ReplaceCelsiusInFile(input string) string {
+import (
+	"bufio"
+	"log"
+	"os"
+)
 
+// function that counts the amout of lines in a file
+func countLines(inputFile string) int {
+	// open file
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()                //closes file
+	scanner := bufio.NewScanner(file) // create scanner from bufio package
+	countedLines := 0                 // intitale variable with amount of lines
+	for scanner.Scan() {              // scan each line for content
+		line := scanner.Text()
+		if line != "" {
+			countedLines++
+		}
+	}
+	return countedLines
+}
+
+func ConvertCelsiusToFahr(input string) string {
+
+	return ""
 }
--- yr/yr_test.go
+++ yr/yr_test.go
@@ -2,6 +2,34 @@ package yr
 
 import "testing"
 
+/*
+	Tester som skal være implementert:
+
+	* antall linjer i filen er 16756
+	* gitt "Kjevik;SN39040;18.03.2022 01:50;6" ønsker å få (want) "Kjevik;SN39040;18.03.2022 01:50;42,8"
+	* gitt "Kjevik;SN39040;07.03.2023 18:20;0" ønsker å få (want) "Kjevik;SN39040;07.03.2023 18:20;32"
+	* gitt "Kjevik;SN39040;08.03.2023 02:20;-11" ønsker å få (want) "Kjevik;SN39040;08.03.2023 02:20;12,2"
+	* gitt "Data er gyldig per 18.03.2023 (CC BY 4.0), Meteorologisk institutt (MET);;;" ønsker å få (want) "Data er basert på gyldig data (per 18.03.2023) (CC BY 4.0) fra Meteorologisk institutt (MET);endringen er gjort av STUDENTENS_NAVN", hvor STUDENTENS_NAVN er navn på studenten som leverer besvarelsen
+	* FOR EKSTRA OPPGAVE: gjennomsnittstemperatur for august 2022 i grader Celsius er ...
+*/
+
+func TestCountLines(t *testing.T) {
+	type test struct {
+		input string
+		want  int
+	}
+
+	var tests = []test{
+		{input: "test-kjevik-temp-celsius-20220318-20230318.csv", want: 16756},
+	}
+	for _, tc := range tests {
+		got := countLines(tc.input)
+
+		if got != tc.want {
+			t.Errorf("%v: want %v, got %v,", tc.input, tc.want, got)
+		}
+	}
+}
 func TestReplaceCelsiusInFile(t *testing.T) {
 
 	//input og resultat ønsket
@@ -13,10 +41,11 @@ func TestReplaceCelsiusInFile(t *testing.T) {
 	var tests = []test{
 		{input: "Kjevik;SN39040;18.03.2023 00:20;4", want: "Kjevik;SN39040;18.03.2023 00:20;39.2"}, // Celsius to Fahr
 		{input: "Kjevik;SN39040;17.03.2023 18:50;3", want: "Kjevik;SN39040;17.03.2023 18:50;37.4"},
+		{input: "Kjevik;SN39040;08.03.2023 02:20;-11", want: "Kjevik;SN39040;08.03.2023 02:20;12,2"},
 		// .... flere tester
 	}
 	for _, tc := range tests {
-		got := ReplaceCelsiusInFile(tc.input)
+		got := ConvertCelsiusToFahr(tc.input)
 
 		if got != tc.want {
 			t.Errorf("%s: want %s, got %s,", tc.input, tc.want, got)
