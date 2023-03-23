a78f840a516a56580fb772ddc6cbb1561f0fd1ff
naausicaa <martinsteiro02@gmail.com>
2023-03-23 14:51:03 +0100

    stilendringer

--- main.go
+++ main.go
@@ -9,6 +9,12 @@ import (
 	"github.com/naausicaa/minyr/yr"
 )
 
+// variables for filenames
+var (
+	inputFile  string = "kjevik-temp-celsius-20220318-20230318.csv"
+	outputFile string = "kjevik-temp-fahr-20220318-20230318.csv"
+)
+
 func main() {
 	fmt.Println("Venligst velg convert, average eller exit:")
 	var input string
@@ -23,7 +29,7 @@ func main() {
 			// Convert file
 		} else if input == "convert" { // funksjon som gjør åpner fil, leser linjer, gjør endringer og lagrer nye linjer i en ny fil
 			// check if file already exists
-			if _, err := os.Stat("kjevik-temp-fahr-20220318-20230318.csv"); err == nil {
+			if _, err := os.Stat(outputFile); err == nil {
 				fmt.Println("converted file already exists. create a new file: y / n")
 				// new scanner for checking if user wants to create a new file or not
 				var input string
@@ -38,23 +44,23 @@ func main() {
 						// if yes - overwrite file
 					} else if input == "y" || input == "yes" {
 						//remove existing file before converting again
-						err := os.Remove("kjevik-temp-fahr-20220318-20230318.csv")
+						err := os.Remove(outputFile)
 						if err != nil {
 							log.Fatal(err)
 						}
-						yr.ConvertFile("kjevik-temp-celsius-20220318-20230318.csv")
-						fmt.Printf("Created file. wrote %v lines.\n", yr.CountLines("kjevik-temp-fahr-20220318-20230318.csv"))
+						yr.ConvertFile(inputFile)
+						fmt.Printf("Created file. wrote %v lines.\n", yr.CountLines(outputFile))
 						os.Exit(0)
 					}
 				}
 			} else {
-				yr.ConvertFile("kjevik-temp-celsius-20220318-20230318.csv")
-				fmt.Printf("Created file. wrote %v lines.\n", yr.CountLines("kjevik-temp-fahr-20220318-20230318.csv"))
+				yr.ConvertFile(inputFile)
+				fmt.Printf("Created file. wrote %v lines.\n", yr.CountLines(outputFile))
 				os.Exit(0)
 			}
 			// averages the temp of file
 		} else if input == "average" {
-			yr.AverageTempOfFile("kjevik-temp-celsius-20220318-20230318.csv")
+			yr.AverageTempOfFile(inputFile)
 		}
 	}
 }
